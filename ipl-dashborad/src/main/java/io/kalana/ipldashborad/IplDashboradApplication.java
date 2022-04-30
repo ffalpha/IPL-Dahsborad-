2d7324a44b2003a1baad9e474aec0de06154f8f6
ffalpha <kalanam217@gmail.com>
2022-04-30 16:14:48 +0530

    Init

---
+++ ipl-dashborad/src/main/java/io/kalana/ipldashborad/IplDashboradApplication.java
@@ -0,0 +1,13 @@
+package io.kalana.ipldashborad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboradApplication.class, args);
+	}
+
+}
---
+++ ipl-dashborad/src/test/java/io/kalana/ipldashborad/IplDashboradApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kalana.ipldashborad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
